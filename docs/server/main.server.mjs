ac890ebc1a35cbe2f1c09276c4703e30f5d368b6
yuri peixoto <86446634+YuriiSouza@users.noreply.github.com>
2023-12-04 14:11:51 -0300

    update

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-7UWMKRXP.mjs";import"./chunk-JYJB7WPM.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  // Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCoxpJf4et3CAef3_bhGxmdTLpXIKFouJw",
+  authDomain: "seuinvestimento.firebaseapp.com",
+  projectId: "seuinvestimento",
+  storageBucket: "seuinvestimento.appspot.com",
+  messagingSenderId: "394071556047",
+  appId: "1:394071556047:web:1b68541fa65248b2859cba",
+  measurementId: "G-QV7081357Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
